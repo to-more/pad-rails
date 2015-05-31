8400b8684e88557a2568d20e69c8e3584fc545ad
to-more <to_more@mailoo.org>
2015-05-31 04:09:23 -0300

    muevo connection a path ./lib/connection/*

--- lib/etherpad_rails.rb
+++ lib/etherpad_rails.rb
@@ -1,5 +1,5 @@
 
-require 'etherpad_rails'
+require 'etherpad_rails/connection/connection.rb'
 
 require 'yaml'
 require 'etherpad-lite'
--- lib/etherpad_rails/connection/connection.rb
+++ lib/etherpad_rails/connection/connection.rb
@@ -2,8 +2,8 @@ module EtherpadRails
   module Connection
     def connect
 
-      config_file = File.dirname(File.expand_path(__FILE__ ) + '/config/etherpad.yml'
-      config = YAML::load File.open(config_file)
+      config_file = 'config/etherpad.yml'
+      config = YAML::load (File.open(config_file))
       
       connected_pad = EtherpadLite.connect config['padurl'], config['apikey'], config['version']
       connected_pad
