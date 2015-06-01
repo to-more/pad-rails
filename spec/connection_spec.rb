eef424a104c0dd491d2b82a6c4d6eda3793e2258
to-more <to_more@mailoo.org>
2015-06-01 06:44:26 -0300

    adding test

---
+++ spec/connection_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+require 'etherpad_rails/connection/connection.rb'
+
+describe EtherpadRails::Connection do
+  let(:including_class) { Class.new { include EtherpadRails::Connection} }
+
+  describe "#connect" do
+    it "when etherpad is connect" do
+      connected_pad = including_class.new.connect
+      expect(connected_pad).not_to eq nil
+    end
+  end  
+end
